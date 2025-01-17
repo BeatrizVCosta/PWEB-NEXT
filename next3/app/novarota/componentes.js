3552e9e90a8f4c5d451000ca7d74e36490917392
Beatriz Costa <beatrizcostavi@gmail.com>
2025-01-17 15:50:45 -0300

    Receita 15

---
+++ next3/app/novarota/componentes.js
@@ -0,0 +1,18 @@
+export function MariaPrea() {
+  return <h2>Morreu Maria Preá...</h2>;
+}
+
+export function ComponenteA() {
+  return <h2>Rota carregada com sucesso</h2>;
+}
+
+export function ComponenteB() {
+  return <h2>Tudo funcionando corretamente</h2>;
+}
+export function Autor() {
+  return <h4>Desenvolvido por Beatriz.</h4>;
+}
+
+export function Message({ text }) {
+  return <h1>{text}</h1>;
+}
---
+++ next3/app/rota3/page.js
@@ -0,0 +1,12 @@
+import { ComponenteA, ComponenteB, Autor } from "../novarota/componentes";
+
+export default function Rota3() {
+  return (
+    <div>
+      <h1>Bem-vindo à Rota 3!</h1>
+      <ComponenteA />
+      <ComponenteB />
+      <Autor />
+    </div>
+  );
+}
